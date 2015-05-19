c32eb561898328f24c5c82f5f6b87aee51a88fbc
gustafbrunlf <gustafbrunlof@gmail.com>
2015-05-19 09:14:59 +0200

    first commit

---
+++ PortfolioGB/Models/Portfolio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace PortfolioGB.Models
+{
+    public class Portfolio
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string About { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class PortfolioDBContext : DbContext
+    {
+        public DbSet<Portfolio> Portfolios { get; set; }
+    }
+}
---
+++ PortfolioGB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PortfolioGB.Startup))]
+namespace PortfolioGB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
